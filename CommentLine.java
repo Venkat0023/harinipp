278063c5ff3cf12b735844a6846711dcfa064717
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-07-22 10:49:41 +0530

    Add files via upload

---
+++ CommentLine.java
@@ -0,0 +1,11 @@
+package logu;
+import java.io.*;
+import java.util.Scanner;
+public class CommentLine {
+	public static void main(String[] args)
+	{
+		System.out.println(args[0] + " technoligies " + args[1]);
+	}
+}
+
+	
