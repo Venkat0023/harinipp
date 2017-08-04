76c443bf61e90b38de238c5a1fca6fb2c37de580
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-08-04 14:56:17 +0530

    Add files via upload

---
+++ MultiplesOfDigits.java
@@ -0,0 +1,21 @@
+package mettl;
+
+import java.util.Scanner;
+
+public class MultiplesOfDigits {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the String");
+		String s=sc.nextLine();
+		int l=s.length();
+		if(l%3==0){
+			int g=l/3;
+		for(int i=3;i<=g;){
+		
+			i+=3;
+		}
+
+	}
+
+	}}
